7e7b2b6feb937584a0b0a74f24f53ed2819a9570
Yan <yans@yancomm.net>
2018-10-30 21:14:15 -0700

    support bundle dirs for arrows

--- archr/arrows/__init__.py
+++ archr/arrows/__init__.py
@@ -5,6 +5,11 @@ import os
 @contextlib.contextmanager
 def bundle(arrow_name):
 	bundle_path = tempfile.mktemp()
-	os.system("%s/%s_bundle.sh %s" % (os.path.dirname(__file__), arrow_name, bundle_path))
+
+	bundle_base_dir = os.path.dirname(__file__)
+	if os.path.exists(os.path.join(bundle_base_dir, arrow_name)):
+		os.system("%s/%s/bundle %s" % (bundle_base_dir, arrow_name, bundle_path))
+	else:
+		os.system("%s/%s_bundle %s" % (bundle_base_dir, arrow_name, bundle_path))
 	yield bundle_path
 	os.unlink(bundle_path)
