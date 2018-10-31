e7d04a9194ba14c1dffbfd7d174f66198a1c6430
Yan <yans@yancomm.net>
2018-10-31 02:13:11 -0700

    make testcase more rigorous

--- tests/test_dockertarget_crazy.py
+++ tests/test_dockertarget_crazy.py
@@ -6,8 +6,10 @@ def setup_module():
 
 def test_false_hook():
     t = archr.targets.DockerImageTarget('archr-test:entrypoint-false').build().start()
+    assert t.run_command().wait() == 1
     with t.shellcode_context(asm_code="mov rax, 60; mov rdi, 42; syscall") as p:
         assert p.wait() == 42
+    assert t.run_command().wait() == 1
     t.stop()
 
 if __name__ == '__main__':
