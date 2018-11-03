9da14128294e238f2afd98e559a457fd5cec5939
Yan <yans@yancomm.net>
2018-11-03 04:38:59 -0700

    make angr happy for now

--- tests/dockers/offsetprinter/offsetprinter.c
+++ tests/dockers/offsetprinter/offsetprinter.c
@@ -15,6 +15,6 @@ int main(int argc, char **argv, char **envp, void *auxp)
 	printf("STDOUT:%p\n", stdout);
 	printf("SMALL_MALLOC:%p\n", malloc(8));
 	printf("BIG_MALLOC:%p\n", malloc(0x313370));
-	printf("MMAP:%p\n", mmap(0, 0x1000, PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0));
+	printf("MMAP:%p\n", mmap(0, 0x1000, PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
 	//write(1, &x, 0x1000);
 }
