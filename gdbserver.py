1353921be7f0f048a8c350a52d688403a1427709
Yan <yans@yancomm.net>
2018-11-23 19:15:32 -0700

    lol

---
+++ gdbserver.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import archr
+import sys
+
+with archr.targets.DockerImageTarget(sys.argv[1]).build() as t:
+	print("target remote %s:1337" % t.ipv4_address)
+	g = archr.arsenal.GDBServerBow(t)
+	g.fire(stdout=1, stderr=2, port=1337, timeout=None)
