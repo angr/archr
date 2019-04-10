9e6263aa92b92ce44b0d175c85f0c2f972ab7674
Audrey Dutcher <audrey@rhelmot.io>
2019-04-10 18:40:08 -0400

    use new nclib feature to solve problems

--- archr/targets/flight.py
+++ archr/targets/flight.py
@@ -75,7 +75,8 @@ class Flight:
 
     def stop(self, timeout=1):
         for sock in self._channels.values():
-            sock.shutdown_wr()
+            if not sock.closed:
+                sock.shutdown_wr()
         if self.process is not None:
             self.process.stdin.close()
             #time.sleep(2)
