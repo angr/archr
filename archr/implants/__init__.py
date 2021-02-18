e0ff284e033ae8533f27117183f7506dd295277e
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2021-02-18 15:53:30 -0500

    made bundles fail on errors to avoid the current case of silent failure

--- archr/implants/__init__.py
+++ archr/implants/__init__.py
@@ -1,27 +1,29 @@
 import contextlib
+import subprocess
 import tempfile
 import shutil
 import os
 
 @contextlib.contextmanager
-def bundle(implant_name):
+def bundle(implant_name: str):
     bundle_path = tempfile.mkdtemp(prefix="archr_bundle_")
 
     bundle_base_dir = os.path.dirname(__file__)
     if os.path.exists(os.path.join(bundle_base_dir, implant_name)):
-        os.system("%s/%s/bundle %s" % (bundle_base_dir, implant_name, bundle_path))
+        subprocess.check_call([f"{bundle_base_dir}/{implant_name}/bundle", bundle_path])
     else:
-        os.system("%s/%s_bundle %s" % (bundle_base_dir, implant_name, bundle_path))
+        subprocess.check_call([f"{bundle_base_dir}/{implant_name}_bundle", bundle_path])
+
     yield bundle_path
 
     shutil.rmtree(bundle_path)
 
 @contextlib.contextmanager
-def bundle_binary(path):
+def bundle_binary(path: str):
     bundle_path = tempfile.mkdtemp(prefix="archr_bundle_binary_")
 
     bundle_base_dir = os.path.dirname(__file__)
-    os.system("%s/GENERIC/bundle %s %s" % (bundle_base_dir, bundle_path, path))
+    subprocess.check_call([f"{bundle_base_dir}/GENERIC/bundle", bundle_path, path])
     yield bundle_path
 
     shutil.rmtree(bundle_path)
