8a760178d3fda57dc4caff4ca43ac566cbfe305e
Yan <yans@yancomm.net>
2021-02-19 02:34:04 -0700

    utility function for finding qemu images

--- tests/common.py
+++ tests/common.py
@@ -1,6 +1,11 @@
 import docker
 import os
 
+TEST_PATH = os.path.dirname(__file__)
+def qemu_test_path(s):
+    return os.path.join(TEST_PATH, "qemus", s)
+
+
 def build_container(name: str):
     build_path = os.path.join(os.path.dirname(__file__), "dockers", name)
     client = docker.from_env()
