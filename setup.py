0ffba1705535da0e7c2ef9acdbe191e6cc5aeebb
Kyle ZENG <jkjh1jkjh1@gmail.com>
2021-05-05 12:17:09 -0700

    pin shellphish-qemu to correct versions

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'archr': ['implants/*.sh', 'implants/*/*']
     },
     install_requires=[
-        'shellphish_qemu',
+        'shellphish_qemu>=0.12.2',
         'pygdbmi',
         'docker',
         'nclib>=1.0.0rc3',
