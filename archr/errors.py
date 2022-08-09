7c64033f2be91b040112ff26a66c9bf98790bdd3
robwaz <rwasinger@asu.edu>
2022-08-09 15:07:41 -0700

    Fix/cleaner exceptions (#121)
    * Raise exception if target_args empty
    
    * define ArchrError
    
    * Raise exception when starting container with invalid entrypoint
    
    * Don't swallow exceptions
    
    * Add ArchrValueError

--- archr/errors.py
+++ archr/errors.py
@@ -1,2 +1,11 @@
 class ArchrError(Exception):
     pass
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f'{self.message}'
+
+class ArchrValueError(ArchrError, ValueError):
+    pass
--- archr/targets/docker_target.py
+++ archr/targets/docker_target.py
@@ -7,6 +7,7 @@ import os
 import re
 
 from . import Target
+from docker.errors import APIError
 
 
 docker = None
@@ -104,6 +105,9 @@ class DockerImageTarget(Target):
             (self.image.attrs['Config']['Entrypoint'] or [ ]) + (self.image.attrs['Config']['Cmd'] or [ ])
         )
 
+        if not self.target_args:
+            raise ArchrValueError("Canot build: target_args is empty!")
+
         # let's assume that we're not analyzing setarch, /bin/sh, or any variant of qemu
         if self.target_args[:2] == [ "/bin/sh", "-c" ]:
             self.target_args = shlex.split(self.target_args[-1])
@@ -144,20 +148,27 @@ class DockerImageTarget(Target):
         else:
             use_init = self.use_init
 
-        self.container = self._client.containers.run(
-            self.image,
-            name=name,
-            entrypoint=entry_point, command=[], environment=self.target_env,
-            user=user, labels=labels,
-            hostname=self.hostname,
-            detach=True, auto_remove=self.rm, working_dir=working_dir,
-            stdin_open=True, stdout=True, stderr=True,
-            privileged=True, security_opt=["seccomp=unconfined"], volumes=self.volumes,
-            network_mode=self.network_mode,
-            network=self.network,
-            init=use_init
-            #network_mode='bridge', ports={11111:11111, self.target_port:self.target_port}
-        )
+        try:
+            self.container = self._client.containers.run(
+                self.image,
+                name=name,
+                entrypoint=entry_point, command=[], environment=self.target_env,
+                user=user, labels=labels,
+                hostname=self.hostname,
+                detach=True, auto_remove=self.rm, working_dir=working_dir,
+                stdin_open=True, stdout=True, stderr=True,
+                privileged=True, security_opt=["seccomp=unconfined"], volumes=self.volumes,
+                network_mode=self.network_mode,
+                network=self.network,
+                init=use_init
+                #network_mode='bridge', ports={11111:11111, self.target_port:self.target_port}
+            )
+        except APIError as e:
+            if f'stat {entry_point[0]}: no such file or directory: unknown' in e.explanation:
+                raise ArchrError(f"Entrypoint not found in container: {entry_point[0]}")
+            else:
+                raise e
+
         self.container.reload()  # update self.container.attrs
 
         if timeout is not None:
